5127d76b6253686cc073fa86569c04e3e81065f4
Matej Pavlicek <tikinang@gmail.com>
2021-07-08 12:54:06 +0200

    test php8

--- public/index.php
+++ public/index.php
@@ -4,6 +4,21 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+class Point {
+  public function __construct(
+    public float $x = 0.0,
+    public float $y = 0.0,
+    public float $z = 0.0,
+  ) {}
+}
+
+htmlspecialchars("haha", double_encode: false);
+
+echo match (8.0) {
+  '8.0' => "Oh no!",
+  8.0 => "This is what I expected",
+};
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
